b79882844765675f928b63afa180374f10c8806b
Janet Leon <janetleon@ce-janets-MacBook-Pro.local>
2021-08-25 14:59:38 -0700

    styles landing

---
+++ src/components/typeform.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import Banner from "./banner"
+// import './styles/OneColumn.scss'
+
+
+const TypeFrom = ({ children }) => {
+
+  return (
+    <div className='TypeForm'>
+      <Banner />
+    </div>
+  )
+
+}
+
+export default TypeFrom
